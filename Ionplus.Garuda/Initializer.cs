9419064929dbcd7cbc2b697c72950783d4b63343
Frank Jordi <frank.jordi@m-f.ch>
2020-01-21 09:32:53 +0100

    Add static initialization method

---
+++ Ionplus.Garuda/Initializer.cs
@@ -0,0 +1,29 @@
+﻿namespace Ionplus.Garuda
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using Ionplus.Garuda.Model;
+
+    public static class Initializer
+    {
+        public static IConnection InitializeConnection(Assembly assembly, InitializeData data)
+        {
+            var connectionType = assembly.GetTypes().SingleOrDefault(t => t.IsInstanceOfType(typeof(IConnection)));
+            if (connectionType == null)
+            {
+                throw new ArgumentException("No implementation of type 'IConnection' found.");
+            }
+
+            var types = new Type[1];
+            types[0] = typeof(InitializeData);
+            var constructor = connectionType.GetConstructor(types);
+            if (connectionType == null)
+            {
+                throw new ArgumentException("Implementation of type 'IConnection' is missing an constructor with 'InitializeData'");
+            }
+
+            return (IConnection)constructor.Invoke(new object[] { data });
+        }
+    }
+}
---
+++ Ionplus.Garuda/Model/InitializeData.cs
@@ -0,0 +1,9 @@
+﻿namespace Ionplus.Garuda.Model
+{
+    public sealed class InitializeData
+    {
+        string DatabaseConnectionString { get; set; }
+        int MachineNumber { get; set; }
+        string Prefix { get; set; }
+    }
+}
