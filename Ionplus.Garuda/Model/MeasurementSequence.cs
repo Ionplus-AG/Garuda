d5a986b84c48b456ed04dbe9e06e90ba133aad53
Frank Jordi <frank.jordi@m-f.ch>
2020-01-22 14:30:19 +0100

    Add sequence to magazine

---
+++ Ionplus.Garuda/Model/MeasurementSequence.cs
@@ -0,0 +1,26 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="MeasurementSequence.cs" company="Ionplus AG">
+// Copyright (c) Ionplus AG. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Ionplus.Garuda.Model
+{
+    /// <summary>
+    /// The measurement sequence model.
+    /// </summary>
+    public sealed class MeasurementSequence
+    {
+        /// <summary>
+        /// Gets or sets the number.
+        /// </summary>
+        public int Number { get; set; }
+
+        /// <summary>
+        /// Gets or sets the position.
+        /// </summary>
+        public int Position { get; set; }
+    }
+}
