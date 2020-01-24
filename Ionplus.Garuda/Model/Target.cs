a4261a8afad4678aa7fcec355dae471f81388b56
Frank Jordi <frank.jordi@m-f.ch>
2020-01-24 08:04:46 +0100

    Add missing target fields

--- Ionplus.Garuda/Model/Target.cs
+++ Ionplus.Garuda/Model/Target.cs
@@ -34,22 +34,47 @@ namespace Ionplus.Garuda.Model
         /// <summary>
         /// Gets or sets the isotope number.
         /// </summary>
-        public int? IsotopeNumber { get; set; }
+        public int IsotopeNumber { get; set; }
 
         /// <summary>
         /// Gets or sets the position.
         /// </summary>
-        public int? Position { get; set; }
+        public int Position { get; set; }
 
         /// <summary>
         /// Gets or sets the maximum cycles.
         /// </summary>
-        public int? MaxCycles { get; set; }
+        public int MaxCycles { get; set; }
 
         /// <summary>
         /// Gets or sets the comment.
         /// </summary>
         public string Comment { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type.
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the code.
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// Gets or sets the code number.
+        /// </summary>
+        public string CodeNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last name.
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is writable.
+        /// </summary>
+        public bool IsWritable { get; set; }
 
         /// <summary>
         /// Gets or sets the runs.
